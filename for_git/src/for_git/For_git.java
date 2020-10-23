5c55690c5299f9e4dcb97e2ca7187ddfa2359b94
DiegoZelaya <69058652+DiegoZelaya@users.noreply.github.com>
2020-10-22 19:37:16 -0600

    Scanner hecho

---
+++ for_git/src/for_git/For_git.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package for_git;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author INTEC
+ */
+public class For_git {
+
+    Scanner scanner=new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
